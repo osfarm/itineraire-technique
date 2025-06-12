f06f7bdf753c14d8b1a6d94c171812344ceb06fa
Nicolas BRENGARD <prodifysas@gmail.com>
2025-06-12 17:07:34 +0200

    User can now export and import an ITK as a file

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ A visualisation tool to show agricultural technical itineraries based on Echarts
 
 # Rendu démo
 Voir : https://osfarm.github.io/itineraire-technique/rendu_statique_1.html
+
+# How to run
+To prepare the project run `npm start`
+Then open `editor.html` with your favorite browser.
---
+++ js/editor-export.js
@@ -0,0 +1,76 @@
+function exportToJsonFile(data, fileName = 'export-itk.json') {
+    const jsonData = JSON.stringify(data, null, 2);
+    const blob = new Blob([jsonData], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    a.click();
+
+    URL.revokeObjectURL(url);
+}
+
+function importFromJsonFile(callback) {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.json';
+
+    input.addEventListener('change', (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                try {
+                    const jsonData = JSON.parse(reader.result);
+                    let parsedCrops = parseCropsFromJson(jsonData);
+                    callback(parsedCrops);
+                } catch (error) {
+                    console.error("Error parsing JSON file:", error);
+                }
+            };
+            reader.readAsText(file);
+        }
+    });
+
+    input.click();
+}
+
+function parseCropsFromJson(cropsFromJson) {
+    return {
+        title: cropsFromJson.title || "",
+        options: cropsFromJson.options || {},
+        steps: cropsFromJson.steps.map(parseStep)
+    };
+}
+
+function parseStep(step) {
+    const crop = new Crop(
+        step.name || "",
+        step.color || "",
+        new Date(step.startDate),
+        new Date(step.endDate),
+        step.description || ""
+    );
+
+    crop.interventions = step.interventions.map(parseIntervention);
+    crop.attributes = step.attributes.map(parseAttribute);
+
+    return crop;
+}
+
+function parseIntervention(intervention) {
+    return new Intervention(
+        intervention.day || 0,
+        intervention.name || "",
+        intervention.type || "",
+        intervention.description || ""
+    );
+}
+
+function parseAttribute(attribute) {
+    return new Attribute(
+        attribute.name || "",
+        attribute.value || ""
+    );
+}
